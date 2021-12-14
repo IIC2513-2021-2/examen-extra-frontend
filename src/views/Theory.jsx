b88f3fe424dbee19b802aeda6befd36601edd26d
Sebastián Vicencio <sivicencio@gmail.com>
2021-12-14 09:50:07 -0300

    feat(app): add theory questions

--- src/views/Theory.jsx
+++ src/views/Theory.jsx
@@ -7,70 +7,70 @@ export default function Theory() {
 
       <div class="question">
         <h3>Pregunta 1</h3>
-        <p></p>
+        <p>¿Para qué sirven los headers HTTP?</p>
         <h4>Respuesta:</h4>
         <p></p>
       </div>
 
       <div class="question">
         <h3>Pregunta 2</h3>
-        <p></p>
+        <p>Supongamos que usted tiene una API RESTful construida y funcionando en producción desde hace un tiempo. Hoy le llega el requerimiento de migrar las URLs existentes a un nuevo esquema (por ejemplo, agregarles un identificador como sufijo). ¿Qué debería hacer para que quienes consumen activamente su API no se vean perjudicados y puedan seguir consumiéndola sin tener que cambiar sus requests?</p>
         <h4>Respuesta:</h4>
         <p></p>
       </div>
 
       <div class="question">
         <h3>Pregunta 3</h3>
-        <p></p>
+        <p>¿A qué se refiere el concepto "Single page application"?</p>
         <h4>Respuesta:</h4>
         <p></p>
       </div>
 
       <div class="question">
         <h3>Pregunta 4</h3>
-        <p></p>
+        <p>Mencione 2 beneficios de llevar a cabo un flujo de desarrollo que incluya Code Review</p>
         <h4>Respuesta:</h4>
         <p></p>
       </div>
 
       <div class="question">
         <h3>Pregunta 5</h3>
-        <p></p>
+        <p>El hook <code>useEffect</code> de React nos sirve sólo para ejecutar un side effect cuando se monta un componente. Comente esta afirmación presentando argumentos</p>
         <h4>Respuesta:</h4>
         <p></p>
       </div>
 
       <div class="question">
         <h3>Pregunta 6</h3>
-        <p></p>
+        <p>En el contexto del desarrollo de un componente React, ¿sería buena idea utilizar <code>document.createElement</code>? Fundamente su respuesta.</p>
         <h4>Respuesta:</h4>
         <p></p>
       </div>
 
       <div class="question">
         <h3>Pregunta 7</h3>
-        <p></p>
+        <p>Las vulnerabilidad de tipo XSS son de las más comunes en la Web. ¿Por qué un ataque XSS persistente puede llegar a afectar a más personas que uno reflejado?</p>
         <h4>Respuesta:</h4>
         <p></p>
       </div>
 
       <div class="question">
         <h3>Pregunta 8</h3>
-        <p></p>
+        <p>¿Cuál es la función de un certificado TLS?</p>
         <h4>Respuesta:</h4>
         <p></p>
       </div>
 
       <div class="question">
         <h3>Pregunta 9</h3>
-        <p></p>
+        <p>¿Cuál es el propósito de un request preflight, en el contexto de CORS?</p>
         <h4>Respuesta:</h4>
         <p></p>
       </div>
 
       <div class="question">
         <h3>Pregunta 10</h3>
-        <p></p>
+        <p>Una persona promedio en su día a día, ¿accede la Web sólo desde un browser? Fundamente su respuesta.</p>
         <h4>Respuesta:</h4>
         <p></p>
       </div>
