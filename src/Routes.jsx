386b786b65cc5bcd499b6f10a251ee610e2fe8b4
Sebastián Vicencio <sivicencio@gmail.com>
2022-03-09 04:34:39 -0300

    feat(expeditions): add NewExpedition empty component

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -6,6 +6,7 @@ import ExpeditionDetail from './views/ExpeditionDetail';
 import Expeditions from './views/Expeditions';
 import Home from './views/Home';
 import Login from './views/Login';
+import NewExpedition from './views/NewExpedition';
 import NewMember from './views/NewMember';
 import NotFound from './views/NotFound';
 import Theory from './views/Theory';
@@ -20,6 +21,7 @@ export default function AppRoutes() {
         <Route path="theory-extra" element={<TheoryExtra />} />
         <Route path="login" element={<Login />} />
         <Route path="expeditions" element={<Expeditions />} />
+        <Route path="expeditions/new" element={<NewExpedition />} />
         <Route path="expeditions/:id" element={<ExpeditionDetail />} />
         <Route path="expeditions/:id/edit" element={<EditExpedition />} />
         <Route path="expeditions/:id/members/new" element={<NewMember />} />
---
+++ src/views/NewExpedition.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function NewExpedition() {
+  return (
+    <div>
+      <h1>New Expedition</h1>
+      <form>
+        <input type="text" placeholder="Change me" />
+      </form>
+    </div>
+  );
+}
